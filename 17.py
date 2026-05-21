ba3b05b2afee61e676021ad12deeff7fd76717ba
guinsooralina <zatcepilamenya1203@gmail.com>
2026-05-21 16:13:20 +0300

    Create 17.py

---
+++ 17.py
@@ -0,0 +1,13 @@
+import boto3
+from botocore.client import Config
+
+ACCESS_KEY = "ВАШ_ИДЕНТИФИКАТОР_КЛЮЧА"
+SECRET_KEY = "ВАШ_СЕКРЕТНЫЙ_КЛЮЧ"
+
+s3_client = boto3.client(
+    's3',
+    endpoint_url='https://storage.yandexcloud.net',
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY,
+    config=Config(region_name='ru-central1', signature_version='s3v4')
+)
